44276f711cb50e752b1ab289cbec4058f8bb2710
gimasha01 <gimashagperera@gmail.com>
2024-09-24 23:56:19 +0530

    Created the bottom information of the page

--- Gemini-Clone/src/components/Main/Main.jsx
+++ Gemini-Clone/src/components/Main/Main.jsx
@@ -36,7 +36,22 @@ const Main = () => {
                 </div>
             </div>
 
-            
+            <div className="main-bottom">
+                <div className="search-box">
+                   <input type="text" placeholder='Enter a prompt here'/>
+                   
+                   
+                    <div>
+                    <img src={assets.gallery_icon} alt="" />
+                    <img src={assets.mic_icon} alt="" />
+                    <img src={assets.send_icon} alt="" />
+                    </div>
+
+                </div>
+                <p className="bottom-info">
+                    Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
+                </p>
+            </div>
 
         </div>
     </div>

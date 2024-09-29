e02f36c611d62dd2424520922fd13096279ea955
gimasha01 <gimashagperera@gmail.com>
2024-09-29 12:16:45 +0530

    Added the state functions

--- Gemini-Clone/src/context/Context.jsx
+++ Gemini-Clone/src/context/Context.jsx
@@ -1,9 +1,18 @@
 import { createContext } from "react";
 import runChat from "../config/gemini";
+import { useState } from 'react';
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
+
+    const [input,setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompts,setPrevPrompts] = useState([]);
+    const [showResult,setShowResult] = useState(false);
+    const [loading,setLoading] = useState(false);
+    const [resultData,setResultData] = useState("");
+
 
     const onSent = async (prompt) =>{
        await runChat(prompt)
@@ -12,7 +21,17 @@ const ContextProvider = (props) => {
     onSent("what is react js")
 
     const contextValue = {
-
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput,
+        newChat
     }
 
     return(

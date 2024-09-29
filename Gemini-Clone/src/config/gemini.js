c0bd658bf58c140c26f546f3c603618df3391c13
gimasha01 <gimashagperera@gmail.com>
2024-09-29 11:55:05 +0530

    created the context.jsx folder

---
+++ Gemini-Clone/src/config/gemini.js
@@ -0,0 +1,59 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+// Replace the environment variable with the actual API key
+const apiKey = "AIzaSyC7ajY6n9CtdN8DAJVxLDn6qusi1RgL_I0";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+// Define safety settings
+const safetySettings = [
+  {
+  category: HarmCategory.HATE,
+  threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+},
+{
+  category: HarmCategory.VIOLENCE,
+  threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+},
+{
+  category: HarmCategory.SELF_HARM,
+  threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+},
+{
+  category: HarmCategory.SEXUAL,
+  threshold: HarmBlockThreshold.BLOCK_HIGH_AND_ABOVE,
+}
+];
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function runChat(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+}
+
+export default runChat;
+
+
--- Gemini-Clone/src/main.jsx
+++ Gemini-Clone/src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import ContextProvider from './context/Context.jsx'
+import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';  // Import StrictMode only once
+import App from "./App.jsx";
+import "./index.css";
+import ContextProvider from "./context/Context.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+
+createRoot(document.getElementById("root")).render(
+  
+  <ContextProvider>
+      <App />
+  </ContextProvider>
+
+);
